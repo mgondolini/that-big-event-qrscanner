714791331b9f27ee839b51986d28baad5b493873
Monica <monica.gondolini@studio.unibo.it>
2021-01-24 23:51:07 +0100

    Added mongoose connection to db

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -13,6 +14,17 @@ app.use(bodyParser.json({ limit: '50MB' }));
 global.log = function log (msg) {
   console.log(msg);
 };
+
+mongoose.connect('mongodb+srv://BEadmin:BEadmin@cluster0.h1eqt.mongodb.net/cluster0?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useFindAndModify', false);
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// TODO insert here the require for every db schema you need
 
 app.use((req, res) => {
   res.status(404).send({ url: `${req.originalUrl} not found` });
