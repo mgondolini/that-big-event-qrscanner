eab61ada39eca8eb241e0a27ccc21c839a7c34be
Monica <monica.gondolini@studio.unibo.it>
2021-01-25 22:34:13 +0100

    Edited mongodb connection string

--- backend/app.js
+++ backend/app.js
@@ -16,11 +16,14 @@ global.log = function log (msg) {
   console.log(msg);
 };
 
-mongoose.connect('mongodb+srv://BEadmin:BEadmin@cluster0.h1eqt.mongodb.net/cluster0?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.set('useFindAndModify', false);
+/**
+ * Mongoose
+ */
+mongoose.connect('mongodb+srv://BEadmin:BEadmin@cluster0.h1eqt.mongodb.net/big-event-demo?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Get the default connection
 const db = mongoose.connection;
+console.log(`dbname${db.db}`);
 
 // Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -30,13 +33,11 @@ require('./model/userModel');
 
 global.appRoot = path.resolve(__dirname);
 
-// Routes
+/**
+ * Routes
+ */
 const router = require('./routes/userRoutes');
-app.use('/routes/userRoutes', router);
-
-app.use((req, res) => {
-  res.status(404).send({ url: `${req.originalUrl} not found` });
-});
+app.use('/', router);
 
 app.listen(3000, () => {
   global.log('Node API server started on port 3000');
