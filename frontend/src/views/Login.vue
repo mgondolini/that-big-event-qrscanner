1a3a4fd6815bcf306d36ed95f8c6db0cdec3d287
Monica <monica.gondolini@studio.unibo.it>
2021-01-27 12:58:27 +0100

    Created Login view

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <b-card title="Card title" sub-title="Card subtitle">
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+                <b-form-group id="input-group-email" label="Your email" label-for="input-email">
+                    <b-form-input
+                        id="input-email"
+                        v-model="form.email"
+                        type="email"
+                        placeholder="Enter email"
+                        required
+                        ></b-form-input>
+                </b-form-group>
+
+                <b-form-group id="input-group-psw" label="Password:" label-for="input-psw">
+                    <b-form-input
+                    id="input-psw"
+                    type="password"
+                    v-model="form.password"
+                    placeholder="Enter password"
+                    required
+                    ></b-form-input>
+                </b-form-group>
+                <b-button type="submit" variant="primary">Submit</b-button>
+            </b-form>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data() {
+        return {
+        form: {
+            email: '',
+            password: ''
+        },
+        }
+    },
+    methods: {
+        onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+        },
+        onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.password = ''
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
