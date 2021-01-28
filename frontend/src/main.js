b263b042eeeb3ed361ba63566716565b41c041b1
Monica <monica.gondolini@studio.unibo.it>
2021-01-28 12:56:23 +0100

    Added Vuex store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Vuex from 'vuex';
 import "./registerServiceWorker";
 import router from './router/router';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
@@ -8,13 +9,22 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(Vuex)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false;
 
+const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  }
+})
+
 new Vue({
   router,
+  store,
   render: function(h) {
     return h(App);
   }
