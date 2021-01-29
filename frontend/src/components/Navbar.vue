5ecdf248a5c7f7beb85424d5a8c69f85d31ab9ad
Monica <monica.gondolini@studio.unibo.it>
2021-01-29 18:31:29 +0100

    Navbar fixed

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,18 +2,18 @@
  <div class=TBEnavbar>
   <b-navbar toggleable="lg" type="dark" variant="info">
 
-    <b-navbar-brand href="#">That Big Event</b-navbar-brand>
+    <b-navbar-brand>That Big Event</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item v-if="this.$store.state.isAuthenticated === true" href="code_scanner">Scan QRcode</b-nav-item>
+          <b-nav-item v-if="this.$store.state.isAuthenticated === true" to="code_scanner">Scan QRcode</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item v-if="this.$store.state.isAuthenticated === true" href="contacts">Contacts</b-nav-item>
+          <b-nav-item v-if="this.$store.state.isAuthenticated === true" to="contacts">Contacts</b-nav-item>
           <b-nav-item-dropdown v-if="this.$store.state.isAuthenticated === true">
             <!-- Using 'button-content' slot -->
             <template #button-content>
